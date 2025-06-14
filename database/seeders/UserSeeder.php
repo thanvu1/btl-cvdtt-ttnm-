6b41ab8c2d7937e9df97d1e06742c1d001051236
thanvu1 <hoangthanvu11@gmail.com>
2025-06-14 14:05:09 +0700

    need breeze

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,31 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin User',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('123456789'),
+                'role' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Customer User',
+                'email' => 'customer@example.com',
+                'password' => Hash::make('123456789'),
+                'role' => 'customer',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
